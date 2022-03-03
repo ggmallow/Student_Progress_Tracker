e8209bc91a0f8236255d81ad16a04a57c5dc3ad1
ggmallow <ggmallow@yahoo.com>
2022-03-02 20:39:14 -0800

    Hooked up start/end date in AddAssement view.

--- app/src/main/java/com/example/studenttracker/UI/AddAssessment.java
+++ app/src/main/java/com/example/studenttracker/UI/AddAssessment.java
@@ -2,15 +2,135 @@ package com.example.studenttracker.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.example.studenttracker.R;
 
+import java.util.Calendar;
+
 public class AddAssessment extends AppCompatActivity {
+
+    private TextView getStart;
+    private TextView getEnd;
+    public DatePickerDialog.OnDateSetListener dateSetListener;
+    public DatePickerDialog datePickerDialog;
+    public DatePickerDialog datePickerDialog2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_assessment);
+
+        getStart = findViewById(R.id.startDate);
+        getEnd = findViewById(R.id.endDate);
+        initStartDatePicker(); // Sets up the date picker for Start Date
+        initEndDatePicker(); //Sets up the date picker for End Date.
+    }
+
+    // This is setting up the Start Date Picker.
+    public void openStartDatePicker(View view) {
+        datePickerDialog.show();
+    }
+
+    public void initStartDatePicker() {
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+
+                month = month + 1;
+
+                String makeDateString = makeDateString(day, month, year);
+                getStart.setText(makeDateString);
+            }
+        };
+        Calendar myCal = Calendar.getInstance();
+        int year = myCal.get(Calendar.YEAR);
+        int month = myCal.get(Calendar.MONTH);
+        int day = myCal.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog = new DatePickerDialog(this, AlertDialog.THEME_HOLO_LIGHT, dateSetListener, year, month, day);
+    }
+
+
+    // This is setting up the End Date Picker.
+    public void openEndDatePicker(View view) {
+        datePickerDialog2.show();
+    }
+
+    public void initEndDatePicker() {
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+
+                month = month + 1;
+
+                String makeDateString = makeDateString(day, month, year);
+                getEnd.setText(makeDateString);
+            }
+        };
+        Calendar myCal = Calendar.getInstance();
+        int year = myCal.get(Calendar.YEAR);
+        int month = myCal.get(Calendar.MONTH);
+        int day = myCal.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog2 = new DatePickerDialog(this, AlertDialog.THEME_HOLO_LIGHT, dateSetListener, year, month, day);
+    }
+
+
+    // Reused by both endDatePicker and startDatePicker
+    private String makeDateString(int day,int month,int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+
+
+    private String getMonthFormat(int month) {
+        if (month == 1) {
+            return "JAN";
+        }
+        if (month == 2) {
+            return "FEB";
+        }
+        if (month == 3) {
+            return "MAR";
+        }
+        if (month == 4) {
+            return "APR";
+        }
+        if (month == 5) {
+            return "MAY";
+        }
+        if (month == 6) {
+            return "JUN";
+        }
+        if (month == 7) {
+            return "JUL";
+        }
+        if (month == 8) {
+            return "AUG";
+        }
+        if (month == 9) {
+            return "SEP";
+        }
+        if (month == 10) {
+            return "OCT";
+        }
+        if (month == 11) {
+            return "NOV";
+        }
+        if (month == 12) {
+            return "DEC";
+        }
+        return "JAN"; //This statement should never be reached.
+
     }
 }
