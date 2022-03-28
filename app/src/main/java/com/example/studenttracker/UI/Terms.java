1ddbb2a5c9e59d99fb2f1d9a7033b550ac2dff33
ggmallow <ggmallow@yahoo.com>
2022-03-28 00:10:04 -0700

    More clean up

--- app/src/main/java/com/example/studenttracker/UI/Terms.java
+++ app/src/main/java/com/example/studenttracker/UI/Terms.java
@@ -10,7 +10,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 
 import android.widget.TextView;
